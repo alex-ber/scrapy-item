150b0657c2da30439db70ad0a7f80c558fe1b161
alex-ber <toalexsmail@yahoo.com>
2020-07-01 21:03:40 +0300

    More test added

--- tests/scrapyitem/general_item_test.py
+++ tests/scrapyitem/general_item_test.py
@@ -4,7 +4,6 @@ logger = logging.getLogger(__name__)
 
 from alexber.scrapyitem import GeneralItem
 
-@pytest.mark.yml
 def test_dynamic_field_creation(request, mocker):
     logger.info(f'{request._pyfuncitem.name}()')
 
@@ -19,7 +18,6 @@ def test_dynamic_field_creation(request, mocker):
     pytest.assume(exp_name == item['name'])
 
 
-@pytest.mark.yml
 def test_dynamic_field_creation_from_dict(request, mocker):
     logger.info(f'{request._pyfuncitem.name}()')
 
@@ -33,6 +31,35 @@ def test_dynamic_field_creation_from_dict(request, mocker):
     pytest.assume(exp_name == item['name'])
 
 
+class YourItem(GeneralItem):
+    pass
+
+
+def test_dynamic_field_creation(request, mocker):
+    logger.info(f'{request._pyfuncitem.name}()')
+
+    exp_id=42
+    exp_name='John'
+
+    item= YourItem()
+    item['id']= exp_id
+    item['name'] = exp_name
+
+    pytest.assume(exp_id==item['id'])
+    pytest.assume(exp_name == item['name'])
+
+
+def test_dynamic_field_creation_from_dict(request, mocker):
+    logger.info(f'{request._pyfuncitem.name}()')
+
+    exp_id=42
+    exp_name='John'
+
+    item= YourItem({'id':exp_id, 'name':exp_name})
+
+
+    pytest.assume(exp_id==item['id'])
+    pytest.assume(exp_name == item['name'])
 
 if __name__ == "__main__":
     pytest.main([__file__])
