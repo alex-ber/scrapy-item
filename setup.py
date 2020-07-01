203a0714e9824cfb555a0c8998df3d367652c36f
alexander_ber <alexander.berkovich@pursway.com>
2020-07-01 16:12:55 +0300

    setup.py changed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 #VERSION should be defined before importing UploadCommand
-VERSION = '0.0.1rc5'
+VERSION = '0.0.1rc6'
 from alexber.utils import UploadCommand
 NAME = 'scrapy-item'
 SHORT_NAME = 'scrapyitem'
